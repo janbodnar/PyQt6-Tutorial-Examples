b84100defdd6d91dd572a4c9519d7c3b58462721
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-17 16:42:04 +0200

    Create tooltip.py

---
+++ first/tooltip.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+This example shows a tooltip on
+a window and a button.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt6.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication)
+from PyQt6.QtGui import QFont
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
