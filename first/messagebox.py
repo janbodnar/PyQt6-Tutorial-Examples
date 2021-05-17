efd7fcab62fc69dc958592c0c467fd8862e6eec7
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-17 16:43:05 +0200

    Create messagebox.py

---
+++ first/messagebox.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+This program shows a confirmation
+message box when we click on the close
+button of the application window.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt6.QtWidgets import QWidget, QMessageBox, QApplication
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 350, 200)
+        self.setWindowTitle('Message box')
+        self.show()
+
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, 'Message',
+                    "Are you sure to quit?", QMessageBox.StandardButtons.Yes |
+                    QMessageBox.StandardButtons.No, QMessageBox.StandardButtons.No)
+
+        if reply == QMessageBox.StandardButtons.Yes:
+
+            event.accept()
+        else:
+
+            event.ignore()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
+    
