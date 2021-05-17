fd2fc6149d3e18aa3d9224a633a835b6cfcffc2c
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-17 16:38:38 +0200

    Create n_of_days.py

---
+++ datetime/n_of_days.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from PyQt6.QtCore import QDate
+
+now = QDate.currentDate()
+
+d = QDate(1945, 5, 7)
+
+print(f'Days in month: {d.daysInMonth()}')
+print(f'Days in year: {d.daysInYear()}')
