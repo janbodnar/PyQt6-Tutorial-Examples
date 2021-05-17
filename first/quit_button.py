83c67b573e813645d7b3cef536214906f5ad3733
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-17 16:42:31 +0200

    Create quit_button.py

---
+++ first/quit_button.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+This program creates a quit
+button. When we press the button,
+the application terminates.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt6.QtWidgets import QWidget, QPushButton, QApplication
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Quit button')
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
+    
