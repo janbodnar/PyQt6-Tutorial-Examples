f5564648bb5011b5d5d5d7144789381ca082b8d7
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-18 09:18:34 +0200

    Create event_object.py

---
+++ events/event_object.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+In this example, we display the x and y
+coordinates of a mouse pointer in a label widget.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QApplication, QGridLayout, QLabel
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        grid = QGridLayout()
+
+        x = 0
+        y = 0
+
+        self.text = f'x: {x},  y: {y}'
+
+        self.label = QLabel(self.text, self)
+        grid.addWidget(self.label, 0, 0, Qt.Alignment.AlignTop)
+
+        self.setMouseTracking(True)
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 450, 300)
+        self.setWindowTitle('Event object')
+        self.show()
+
+
+    def mouseMoveEvent(self, e):
+
+        x = int(e.position().x())
+        y = int(e.position().y())
+
+        text = f'x: {x},  y: {y}'
+        self.label.setText(text)
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
+ 
