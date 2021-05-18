c55b557cfb68e7cff272d3126ec173a7f091ff24
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-18 09:27:45 +0200

    Create splitter.py

---
+++ widgets2/splitter.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+This example shows
+how to use QSplitter widget.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QWidget, QHBoxLayout, QFrame,
+        QSplitter, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        hbox = QHBoxLayout(self)
+
+        topleft = QFrame(self)
+        topleft.setFrameShape(QFrame.Shape.StyledPanel)
+
+        topright = QFrame(self)
+        topright.setFrameShape(QFrame.Shape.StyledPanel)
+
+        bottom = QFrame(self)
+        bottom.setFrameShape(QFrame.Shape.StyledPanel)
+
+        splitter1 = QSplitter(Qt.Orientations.Horizontal)
+        splitter1.addWidget(topleft)
+        splitter1.addWidget(topright)
+
+        splitter2 = QSplitter(Qt.Orientations.Vertical)
+        splitter2.addWidget(splitter1)
+        splitter2.addWidget(bottom)
+
+        hbox.addWidget(splitter2)
+        self.setLayout(hbox)
+
+        self.setGeometry(300, 300, 450, 400)
+        self.setWindowTitle('QSplitter')
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
+    
