d2350b4c5c5e0a5cbcb1c021f6b5f2ed275da0f8
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-18 09:17:32 +0200

    Create signals_slots.py

---
+++ events/signals_slots.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+In this example, we connect a signal
+of a QSlider to a slot of a QLCDNumber.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QWidget, QLCDNumber, QSlider,
+        QVBoxLayout, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        lcd = QLCDNumber(self)
+        sld = QSlider(Qt.Orientations.Horizontal, self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+        sld.valueChanged.connect(lcd.display)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Signal and slot')
+        self.show()
+
+
+def main():
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
