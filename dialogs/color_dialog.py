12c05bb998393b3209ef0bdd628d6a5f98eeb137
Jan Bodnar <jan.bodnar@gmail.com>
2021-05-18 09:21:15 +0200

    Create color_dialog.py

---
+++ dialogs/color_dialog.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+"""
+ZetCode PyQt6 tutorial
+
+In this example, we select a color value
+from the QColorDialog and change the background
+color of a QFrame widget.
+
+Author: Jan Bodnar
+Website: zetcode.com
+"""
+
+from PyQt6.QtWidgets import (QWidget, QPushButton, QFrame,
+        QColorDialog, QApplication)
+from PyQt6.QtGui import QColor
+import sys
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        col = QColor(0, 0, 0)
+
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.showDialog)
+
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }"
+                               % col.name())
+        self.frm.setGeometry(130, 22, 200, 200)
+
+        self.setGeometry(300, 300, 450, 350)
+        self.setWindowTitle('Color dialog')
+        self.show()
+
+
+    def showDialog(self):
+
+        col = QColorDialog.getColor()
+
+        if col.isValid():
+
+            self.frm.setStyleSheet("QWidget { background-color: %s }" 
+                                   % col.name())
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
